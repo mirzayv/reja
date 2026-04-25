2a1478daec3a5973c731fb174c498fb8a8e8e4e9
mirzayv <mirzayv919@gmail.com>
2026-04-25 18:16:33 +0900

    feat: MITASK A

--- train.js
+++ train.js
@@ -1,21 +1,41 @@
-// B-TASK
-// A-TASK
+// Masalani izohi
+// A-TASK:
+
+// Shunday 2 parametrli function tuzing,
+// hamda birinchi parametrdagi harfni ikkinchi parametrdagi so‘zdan
+// necha marta qatnashganini return qilishi kerak.
+
+// Masalan: countLetter("e", "engineer") → 3
+
+// masalani yechimi:
+function countLetter(letter, word) {
+  let count = 0;
+
+  for (let i = 0; i < word.length; i++) {
+    if (word[i] === letter) {
+      count++;
+    }
+  }
+  return count;
+}
+console.log(countLetter("e", "engineer")); // 3
+
 //console.log('train task ishga tushdi!');
 //D-TASK
 /* console.log("TRAIN AREA!"); */
 //D-TASK
 
-console.log("Jack Ma maslahatlari");
-const list = [
+// console.log("Jack Ma maslahatlari");
+/* const list = [
   "yahshi talaba boling", // 0-20
   "togri boshliq tanlang va koproq xato qiling", // 20-30
   "uzingizni ishlashingizni boshlang", // 30-40
   "siz kuchli bolgan narsalarni qiling", // 40-50
   "yoshlarga investitsiya qiling", // 50-60
   "endi dam oling, foydasi yoq endi", // 60
-];
+]; */
 // callback function
-function maslahatBering(a, callback) {
+/* function maslahatBering(a, callback) {
   if (typeof a !== "number") callback("Insert a number", null);
   else if (a <= 20) callback(null, list[0]);
   else if (a < 20 && a <= 30) callback(null, list[1]);
@@ -39,7 +59,7 @@ maslahatBering(70, (err, data) => {
 });
 
 console.log("passed here 1");
-
+ */
 /* function maslahatBering(a, callback) {
   if (typeof a !== "number") callback("insert a number", null);
   else if (a <= 20) callback(null, list[0]);
