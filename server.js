3cf4726a7c05ccd41f5da01b5c7971fd8872387e
mirzayv <mirzayv919@gmail.com>
2026-04-28 16:52:03 +0900

    feat update: update connection string

--- server.js
+++ server.js
@@ -3,27 +3,23 @@ const mongodb = require("mongodb");
 
 let db;
 const connectionString =
-  "mongodb+srv://komron:OYWZcKc03ZEljF02@cluster0.jhplugm.mongodb.net/Reja?appName=Cluster0";
+  "mongodb+srv://komron:OYWZcKc03ZEljF02@cluster0.jhplugm.mongodb.net/Reja?retryWrites=true&w=majority";
 
 mongodb.connect(
   connectionString,
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  },
+  { useNewUrlParser: true, useUnifiedTopology: true },
   (err, client) => {
-    if (err) console.log("ERROR on connection MongoDB");
+    if (err) console.log("ERRORon connection MongoDB");
     else {
       console.log("MongoDB connection succeed");
       module.exports = client;
 
-      //console.log(client);
       const app = require("./app");
       const server = http.createServer(app);
       let PORT = 3000;
       server.listen(PORT, function () {
         console.log(
-          `The server is running successfully on port: ${PORT}, http://localhost:${PORT}`,
+          `The server running successfully on port: ${PORT}, https://localhost:${PORT}`,
         );
       });
     }
