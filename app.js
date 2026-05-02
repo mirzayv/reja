f9a80bfbeb2c9ff983bb9e6ba70d256cc20f5185
mirzayv <mirzayv919@gmail.com>
2026-05-02 16:03:08 +0900

    feat: delete functionality and frontend integration

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const app = express();
 //MongoDB connect
 //MongoDB chaqirish
 const db = require("./server").db();
+const mongodb = require("mongodb");
 
 /* let user;
 fs.readFile("database/user.json", "utf8", (err, data) => {
@@ -61,6 +62,16 @@ app.post("/create-item", (req, res) => {
     console.log(data.ops);
     res.json(data.ops[0]);
   });
+});
+
+app.post("/delete-item", (req, res) => {
+  const id = req.body.id;
+  db.collection("plans").deleteOne(
+    { _id: new mongodb.ObjectId(id) },
+    function (err, data) {
+      res.json({ state: "success" });
+    },
+  );
 });
 
 app.get("/", function (req, res) {
--- public/browser.js
+++ public/browser.js
@@ -1,3 +1,5 @@
+//const { response } = require("../app");
+
 console.log("FrontEnd JS ishga tushdi");
 
 function itemTemplate(item) {
@@ -36,4 +38,43 @@ document.getElementById("create-form").addEventListener("submit", function (e) {
     .catch((err) => {
       console.log("Iltimos qaytadan harakat qiling!");
     });
+});
+
+/* document.addEventListener("click", function (e) {
+  console.log(e);
+
+  if (e.target.classList.contains("delete-me")) {
+    alert("siz delete tugmasini bosdingiz");
+  }
+
+  if (e.target.classList.contains("edit-me")) {
+    alert("siz edit tugmasini bosdingiz");
+  }
+});
+ */
+
+document.addEventListener("click", function (e) {
+  // delete oper
+  console.log(e.target);
+  if (e.target.classList.contains("delete-me")) {
+    if (confirm("Aniq ochirmoqchimisiz?")) {
+      /*    alert("Yes deb javob berildi");
+    } else {
+      alert("No deb javob berildi"); */
+      axios
+        .post("/delete-item", { id: e.target.getAttribute("data-id") })
+        .then((response) => {
+          console.log(response.data);
+          e.target.parentElement.parentElement.remove();
+        })
+        .catch((err) => {
+          console.log("Iltimos qaytadan harakat qiling!");
+        });
+    }
+  }
+
+  // edit oper
+  if (e.target.classList.contains("edit-me")) {
+    alert("siz edit tugmasini bosdingiz");
+  }
 });
