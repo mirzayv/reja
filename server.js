b2707bffd29148e69949a303a49678a7aba4978e
mirzayv <mirzayv919@gmail.com>
2026-05-05 14:10:51 +0900

    feat: Reja loyihamizning yakuniy bosqich logic

--- server.js
+++ server.js
@@ -1,18 +1,20 @@
 const http = require("http");
-const mongodb = require("mongodb");
+const mongodb = require("mongodb"); // OBJECT
 
 let db;
 const connectionString =
   "mongodb+srv://komron:OYWZcKc03ZEljF02@cluster0.jhplugm.mongodb.net/Reja?retryWrites=true&w=majority";
 
+// TCP (doimiy boglanish) vs HTTP (1marttalik request)
 mongodb.connect(
   connectionString,
   { useNewUrlParser: true, useUnifiedTopology: true },
+  // callback boglanish
   (err, client) => {
     if (err) console.log("ERRORon connection MongoDB");
     else {
       console.log("MongoDB connection succeed");
-      module.exports = client;
+      module.exports = client; //OBJECT
 
       const app = require("./app");
       const server = http.createServer(app);
